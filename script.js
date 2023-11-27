1321843515f4716791f291740bcb460fcac893b1
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-28 00:10:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function toCase(text) {
   // write your code here
+	let str1=text.toLowerCase();
+	let str=text.toUpperCase()
+	let str3=str1;
+	str3=str3+"-";
+	str3=str3+str;
+	return str3;
 }
 
 // DO not change the code below
